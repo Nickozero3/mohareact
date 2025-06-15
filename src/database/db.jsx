f4b4f6302d72616dd742397b89ccdbaa1095e577
Nicolas Ochoa <Nickoochoa03@gmail.com>
2025-06-14 22:50:00 -0300

    chore: Add engines field to package.json and package-lock.json for Node and npm version requirements

--- src/database/db.jsx
+++ src/database/db.jsx
@@ -2,12 +2,13 @@
 
 const mysql = require('mysql2/promise')
 
-
+//mysql://root:vCtTosbYRiFVRmCflclIbPFzkJeSkbPQ@caboose.proxy.rlwy.net:42356/railway 
 const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || '',
+    port: process.env.DB_PORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
 })
