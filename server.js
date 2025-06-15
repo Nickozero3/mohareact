85131d2aa1ced38617bb3ec446a2deff1c55a5a8
Nicolas Ochoa <Nickoochoa03@gmail.com>
2025-06-15 00:10:29 -0300

    fix: Update MySQL database name fallback logic in connection pool configuration

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "railway",
+  database: process.env.DB_NAME || "cellstore_bd" || "railway",
   waitForConnections: true,
   connectionLimit: 10,
 });
